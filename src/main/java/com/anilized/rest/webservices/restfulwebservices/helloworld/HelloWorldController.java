8add2d18e39e50c4cfe88a95de699caad8d270a1
Anıl Can <anillcan7@gmail.com>
2021-02-22 20:09:06 +0300

    Internalization added for HelloWorld app

--- src/main/java/com/anilized/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/anilized/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.anilized.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	//GET
 	//URI - /hello-world
@@ -24,6 +32,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean/path-var/{name}")
 	public HelloWorldPathVar helloWorldPathVar(@PathVariable String name){
 		return new HelloWorldPathVar(String.format("Hello World, %s", name ));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String internationalizedHelloWorldBean(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 
 }
